f6cbb35a25149bc6afbea0a6cfb189c2c201c09f
rohit1039 <rohitparida0599@gmail.com>
2024-04-14 23:02:49 +0530

    code refactored

---
+++ order-service/src/main/java/com/ecommerce/orderservice/constant/APIConstants.java
@@ -0,0 +1,15 @@
+package com.ecommerce.orderservice.constant;
+
+public class APIConstants {
+
+  public static final String PRODUCT_SERVICE = "/products/v1/order-product/";
+  public static final String PRODUCT_QUANTITY = "quantity";
+  public static final String HOSTNAME = "localhost";
+  public static final Integer PRODUCT_PORT = 8083;
+  public static final String PRODUCT_SIZE = "productSize";
+  public static final String CONTENT_TYPE = "Content-Type";
+  public static final String JSON_CONTENT_TYPE = "application/json";
+  public static final Integer SUCCESS_STATUS_CODE = 200;
+  public static final Integer ERROR_STATUS_CODE = 500;
+  public static final Integer CREATED_STATUS_CODE = 201;
+}
