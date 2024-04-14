dd92f5cf5f9e4e8ca1a28a4edfc618e5c84510ea
rohit1039 <rohitparida0599@gmail.com>
2024-04-14 23:02:49 +0530

    refactored code

--- order-service/src/main/java/com/ecommerce/orderservice/payload/request/OrderRequest.java
+++ order-service/src/main/java/com/ecommerce/orderservice/payload/request/OrderRequest.java
@@ -1,6 +1,17 @@
 package com.ecommerce.orderservice.payload.request;
 
-// productId, userId, quantity, productSize,
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OrderRequest {
 
   private String fullName;
--- order-service/src/main/java/com/ecommerce/orderservice/util/OrderHandler.java
+++ order-service/src/main/java/com/ecommerce/orderservice/util/OrderHandler.java
@@ -1,5 +1,10 @@
 package com.ecommerce.orderservice.util;
 
+import static com.ecommerce.orderservice.constant.APIConstants.*;
+
+import com.ecommerce.orderservice.payload.request.OrderRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava3.core.MultiMap;
@@ -19,29 +24,42 @@ public class OrderHandler implements Handler<RoutingContext> {
   public void handle(RoutingContext routingContext) {
 
     JsonObject requestBody = routingContext.body().asJsonObject();
+    ObjectMapper objectMapper = new ObjectMapper();
+    OrderRequest orderRequest = requestBody.mapTo(OrderRequest.class);
     JsonObject response = new JsonObject();
     long productId = Long.parseLong(routingContext.request().getParam("productId"));
     MultiMap entries = routingContext.request().params();
     LOG.info("*** Calling product-service with productId: {} ***", productId);
+    updateProductCount(routingContext, response, productId, entries);
+    try {
+      LOG.info("\n Incoming request: {}", objectMapper.writeValueAsString(orderRequest));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void updateProductCount(
+      RoutingContext routingContext, JsonObject response, long productId, MultiMap entries) {
+
     vertxClient
-        .put(8083, "localhost", "/products/v1/order-product/" + productId)
-        .addQueryParam("quantity", entries.get("quantity"))
-        .addQueryParam("productSize", entries.get("productSize"))
+        .put(PRODUCT_PORT, HOSTNAME, PRODUCT_SERVICE + productId)
+        .addQueryParam(PRODUCT_QUANTITY, entries.get(PRODUCT_QUANTITY))
+        .addQueryParam(PRODUCT_SIZE, entries.get(PRODUCT_SIZE))
         .rxSend()
         .doOnSuccess(
             apiResponse -> {
-              if (apiResponse.statusCode() == 200) {
+              if (apiResponse.statusCode() == SUCCESS_STATUS_CODE) {
                 response.put("response", "Order placed successfully!");
                 routingContext
                     .response()
-                    .setStatusCode(201)
-                    .putHeader("content-type", "application/json")
+                    .setStatusCode(CREATED_STATUS_CODE)
+                    .putHeader(CONTENT_TYPE, JSON_CONTENT_TYPE)
                     .end(response.encodePrettily());
               } else {
                 routingContext
                     .response()
                     .setStatusCode(apiResponse.statusCode())
-                    .putHeader("content-type", "application/json")
+                    .putHeader(CONTENT_TYPE, JSON_CONTENT_TYPE)
                     .end();
                 LOG.error("Some error occurred in Product Service: {}", apiResponse.statusCode());
               }
@@ -61,6 +79,5 @@ public class OrderHandler implements Handler<RoutingContext> {
                     message.bodyAsJsonObject() != null
                         ? message.bodyAsJsonObject().encodePrettily()
                         : message.statusCode()));
-    LOG.info("\n Incoming request: {}", requestBody.encode());
   }
 }
