d2ca223dcec4ad696997a2dd6c5919abbcb0da06
rohit1039 <rohitparida0599@gmail.com>
2024-11-29 20:54:32 +0530

    updated routes

--- order-service/src/main/java/com/ecommerce/orderservice/constant/ApiConstants.java
+++ order-service/src/main/java/com/ecommerce/orderservice/constant/ApiConstants.java
@@ -11,9 +11,9 @@ public class ApiConstants {
   public static final String ORDER_ID = "_id";
   public static final String ORDER_STATS = "order_status";
   public static final String PRODUCT_ID = "product_id";
-  public static final String PLACE_ORDER_ENDPOINT = "/v1/place-order";
-  public static final String GET_ALL_ORDERS_ENDPOINT = "v1/orders"; // only by admins
-  public static final String GET_ORDERS_BY_USER_ENDPOINT = "/v1/get-orders";
+  public static final String PLACE_ORDER_ENDPOINT = "/orders/place-order";
+  public static final String GET_ALL_ORDERS_ENDPOINT = "orders/all"; // only by admins
+  public static final String GET_ORDERS_BY_USER_ENDPOINT = "/orders/get-orders";
   public static final String ORDER_ITEMS = "order_items";
 
   public static final String ORDER_PLACED_BY = "order_placed_by";
